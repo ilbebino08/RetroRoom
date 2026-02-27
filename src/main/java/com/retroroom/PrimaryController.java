833bb57020fe105d351a87970153cd6059ab45b2
Ilbebino <154010870+ilbebino08@users.noreply.github.com>
2026-02-27 11:37:35 +0100

    first commit
    creazione del progetto

---
+++ src/main/java/com/retroroom/PrimaryController.java
@@ -0,0 +1,12 @@
+package com.retroroom;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
